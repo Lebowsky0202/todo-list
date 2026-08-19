00b984694cfb84874c8ef91cbbaa45e15305b97d
Kuat <tipoproger@gmail.com>
2026-08-19 10:07:34 +0500

    feat: search update

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
 import cls from './Search.module.css'
 
 const Search = ({ setIsOpen }) => {
+	const [searchName, setSearchName] = useState('')
+
+	function handleSearchName(value) {
+		setSearchName(value)
+	}
+
 	return (
 		<div className={cls.search}>
-			<input type='text' placeholder='Search tasks' />
+			<input
+				type='text'
+				placeholder='Search tasks'
+				onChange={e => handleSearchName(e.target.value)}
+			/>
 			<button onClick={() => setIsOpen(true)}>+</button>
 		</div>
 	)
