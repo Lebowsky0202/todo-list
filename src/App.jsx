57fa9db0b40393ee20177634365404d19bc1ad33
Kuat <tipoproger@gmail.com>
2026-08-20 10:14:23 +0500

    feat: добавлена сортировка задач по поиску

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,21 @@ function App() {
 		return savedTasks ? JSON.parse(savedTasks) : []
 	})
 
+	const [search, setSearch] = useState('')
+
 	useEffect(() => {
 		localStorage.setItem('tasks', JSON.stringify(tasks))
 	}, [tasks])
 
+	const filteredTasks = tasks.filter(task =>
+		task.name.toLowerCase().includes(search.toLowerCase()),
+	)
+
 	return (
 		<div>
 			<h1>To Do List</h1>
-			<Search setIsOpen={setIsOpen} />
-			<Tasks tasks={tasks} setTasks={setTasks} />
+			<Search setIsOpen={setIsOpen} search={search} setSearch={setSearch} />
+			<Tasks tasks={filteredTasks} setTasks={setTasks} />
 			<Modal
 				isOpen={isOpen}
 				isClose={() => setIsOpen(false)}
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
 import cls from './Search.module.css'
 
-const Search = ({ setIsOpen }) => {
-	const [searchName, setSearchName] = useState('')
+const Search = ({ setIsOpen, search, setSearch }) => {
+	// const [searchName, setSearchName] = useState('')
 
 	function handleSearchName(value) {
-		setSearchName(value)
+		setSearch(value)
 	}
 
 	return (
 		<div className={cls.search}>
 			<input
 				type='text'
+				value={search}
 				placeholder='Search tasks'
 				onChange={e => handleSearchName(e.target.value)}
 			/>
